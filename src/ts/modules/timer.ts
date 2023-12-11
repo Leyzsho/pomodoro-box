0a6fe7cbf5441a4307e1f3179d3d58820cf7fa40
Michael <xcozziw@gmail.com>
2023-12-11 19:39:40 +0300

    feat: add break

--- src/ts/modules/timer.ts
+++ src/ts/modules/timer.ts
@@ -1,12 +1,12 @@
 import Task, { ITaskInfo } from './task';
 import timeEndSound from '../../audio/time-end-sound.mp3';
+import breakTimeEndSound from '../../audio/breakTimeEndSound.mp3';
 import { gsap } from 'gsap';
 
 export enum TIME_STATUS {
   WORK = 'work',
   READINESS = 'readiness',
-  SHORT_BREAK = 'short-break',
-  BIG_BREAK = 'big-break',
+  BREAK = 'break',
   PAUSE = 'pause',
 }
 
@@ -15,21 +15,29 @@ enum FIRST_BTN_STATUS {
   PAUSE = 'pause',
   CONTINUE = 'continue',
   START_BREAK = 'start-break',
+  PAUSE_BREAK = 'pause-break',
+  CONTINUE_BREAK = 'continue-break',
 }
 
 enum SECOND_BTN_STATUS {
   STOP = 'stop',
-  SKIP = 'skip',
+  SKIP_BREAK = 'skip-break',
   DONE = 'done',
 }
 
 export class Timer {
   // time properties
 
-  private static timeElement: HTMLSpanElement | null = document.querySelector('.app__task-time');
-  private static time: number;
+  private static currentTime: number;
+  private static __defaultTime: number = 10;
+  private static __defaultTimeOfCurrentTask: number = 10;
   private static TimeInterval: NodeJS.Timeout;
+
+  private static timeElement: HTMLSpanElement | null = document.querySelector('.app__task-time');
+  private static addTimeBtn: HTMLButtonElement | null = document.querySelector('.app__task-add-time');
+
   private static timeEndSound: HTMLAudioElement = document.createElement('audio');
+  private static breakTimeEndSound: HTMLAudioElement = document.createElement('audio');
 
   // tomato properties
 
@@ -59,14 +67,16 @@ export class Timer {
 
   // readiness methods
 
-  static defaultPropertiesReset(): void {
+  static defaultResetBeforeReadiness(): void {
     Timer.firstBtnStatus = FIRST_BTN_STATUS.START;
     Timer.secondBtnStatus = SECOND_BTN_STATUS.STOP;
+    Timer.addTimeBtn ? Timer.addTimeBtn.disabled = false : '';
 
     clearInterval(Timer.TimeInterval);
-    Timer.setTime();
+    Timer.setTimeByDefault();
 
     Timer.timeEndSound.remove();
+    Timer.breakTimeEndSound.remove();
 
     Timer.currentTomato = 1;
 
@@ -75,7 +85,7 @@ export class Timer {
 
   static setTaskForReadiness(task: ITaskInfo): void {
     if (Timer.timeStatus === TIME_STATUS.READINESS) {
-      Timer.defaultPropertiesReset();
+      Timer.defaultResetBeforeReadiness();
       Timer.taskInfo = task;
 
       const title: HTMLTitleElement | null = document.querySelector('.app__task-title');
@@ -88,10 +98,6 @@ export class Timer {
       taskInfoIntroduction ? taskInfoIntroduction.textContent = 'Задача 1 - ' : '';
       taskInfoName ? taskInfoName.textContent = task.name : '';
 
-      const addTimeBtn: HTMLButtonElement | null = document.querySelector('.app__task-add-time');
-
-      addTimeBtn ? addTimeBtn.disabled = false : '';
-
       if (Timer.firstBtn) {
         Timer.firstBtn.textContent = 'Старт';
         Timer.firstBtn.disabled = false;
@@ -99,20 +105,18 @@ export class Timer {
         Timer.firstBtn.onclick = () => {
           if (Timer.firstBtnStatus === FIRST_BTN_STATUS.START) {
             Timer.start();
-
-            Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE;
-            Timer.firstBtn ? Timer.firstBtn.textContent = 'Пауза' : '';
           } else if (Timer.firstBtnStatus === FIRST_BTN_STATUS.PAUSE) {
             Timer.pause();
-
-            Timer.firstBtnStatus = FIRST_BTN_STATUS.CONTINUE;
-            Timer.firstBtn ? Timer.firstBtn.textContent = 'Продолжить' : '';
           } else if (Timer.firstBtnStatus === FIRST_BTN_STATUS.CONTINUE) {
             Timer.continue();
-
-            Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE;
-            Timer.firstBtn ? Timer.firstBtn.textContent = 'Пауза' : '';
+          } else if (Timer.firstBtnStatus === FIRST_BTN_STATUS.START_BREAK) {
+            Timer.startBreak();
+          } else if (Timer.firstBtnStatus === FIRST_BTN_STATUS.PAUSE_BREAK) {
+            Timer.pauseBreak();
+          } else if (Timer.firstBtnStatus === FIRST_BTN_STATUS.CONTINUE_BREAK) {
+            Timer.continueBreak();
           }
+
           Timer.secondBtn ? Timer.secondBtn.disabled = false : '';
         };
       }
@@ -124,17 +128,17 @@ export class Timer {
         Timer.secondBtn.onclick = () => {
           if (Timer.secondBtnStatus === SECOND_BTN_STATUS.STOP) {
             Timer.stop();
-
-            Timer.firstBtnStatus = FIRST_BTN_STATUS.START;
-            Timer.firstBtn ? Timer.firstBtn.textContent = 'Старт' : '';
-            Timer.secondBtn ? Timer.secondBtn.disabled = true : '';
           } else if (Timer.secondBtnStatus === SECOND_BTN_STATUS.DONE) {
-            Timer.timeEndSound.remove();
-
-            Timer.timeStatus = TIME_STATUS.READINESS;
-
-            Task.removeTaskById(Timer.taskInfo.id);
+            Timer.done();
+          } else if (Timer.secondBtnStatus === SECOND_BTN_STATUS.SKIP_BREAK) {
+            Timer.skipBreak();
           }
+        };
+      }
+
+      if (Timer.addTimeBtn) {
+        Timer.addTimeBtn.onclick = () => {
+          Timer.addTimeForCurrentTask(60);
         };
       }
 
@@ -143,7 +147,7 @@ export class Timer {
   }
 
   static clearReadiness(): void {
-    Timer.defaultPropertiesReset();
+    Timer.defaultResetBeforeReadiness();
 
     const title: HTMLTitleElement | null = document.querySelector('.app__task-title');
     const currentTomato: HTMLSpanElement | null = document.querySelector('.app__task-current-tomato');
@@ -155,7 +159,7 @@ export class Timer {
     taskInfoIntroduction ? taskInfoIntroduction.textContent = '' : '';
     taskInfoName ? taskInfoName.textContent = '' : '';
 
-    const addTimeBtn: HTMLButtonElement | null = document.querySelector('.app__task-add-time');
+    Timer.addTimeBtn ? Timer.addTimeBtn.disabled = true : '';
 
     if (Timer.firstBtn) {
       Timer.firstBtn.disabled = true;
@@ -167,22 +171,32 @@ export class Timer {
       Timer.secondBtn.textContent = 'Стоп';
     }
 
-    addTimeBtn ? addTimeBtn.disabled = true : '';
-
     localStorage.removeItem('current-task-id');
   }
 
-  // time methods
+  // general time methods
 
-  private static setTime(): void {
-    Timer.time = 10;
+  private static setTimeByDefault(): void {
+    Timer.__defaultTimeOfCurrentTask = Timer.__defaultTime;
+    Timer.currentTime = Timer.__defaultTime;
+    Timer.timeElement ? Timer.timeElement.textContent = Timer.getNormalTimeFormat() : '';
+  }
+
+  private static setTimeOfCurrentTask(): void {
+    Timer.currentTime = Timer.__defaultTimeOfCurrentTask;
+    Timer.timeElement ? Timer.timeElement.textContent = Timer.getNormalTimeFormat() : '';
+  }
+
+  private static addTimeForCurrentTask(seconds: number): void {
+    Timer.__defaultTimeOfCurrentTask = Timer.currentTime + seconds;
+    Timer.currentTime = Timer.__defaultTimeOfCurrentTask;
     Timer.timeElement ? Timer.timeElement.textContent = Timer.getNormalTimeFormat() : '';
   }
 
   private static getNormalTimeFormat(): string {
     let minutes: number = 0;
     let seconds: number = 0;
-    let time = Timer.time;
+    let time = Timer.currentTime;
 
     if (time > 59) {
       while (time > 59) {
@@ -201,9 +215,13 @@ export class Timer {
     }
   }
 
+  // task time
+
   private static taskCompleted(): void {
     Timer.playTimeEndSound();
     Timer.taskInfo.tomatoCount -= 1;
+
+    Timer.addTimeBtn ? Timer.addTimeBtn.disabled = true : '';
 
     gsap.set(Timer.timeElement, { fontSize: 90 });
     Timer.timeElement ? Timer.timeElement.textContent = 'Время вышло' : '';
@@ -237,57 +255,167 @@ export class Timer {
   }
 
   private static start(): void {
+    Timer.breakTimeEndSound.remove();
     Timer.timeStatus = TIME_STATUS.WORK;
-    Timer.setTime();
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE;
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Пауза' : '';
+
+    const title: HTMLTitleElement | null = document.querySelector('.app__task-title');
+    const currentTomato: HTMLSpanElement | null = document.querySelector('.app__task-current-tomato');
+
+    title ? title.textContent = Timer.taskInfo.name : '';
+    currentTomato ? currentTomato.textContent = `Помидор: ${Timer.currentTomato}` : '';
+
+    Timer.addTimeBtn ? Timer.addTimeBtn.disabled = false : '';
+
+    Timer.setTimeOfCurrentTask();
     Timer.tick();
   }
 
   private static continue(): void {
     Timer.timeStatus = TIME_STATUS.WORK;
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE;
+    Timer.secondBtnStatus = SECOND_BTN_STATUS.STOP;
+
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Пауза' : '';
+    Timer.secondBtn ? Timer.secondBtn.textContent = 'Стоп' : '';
+
     Timer.tick();
   }
 
   private static pause(): void {
     Timer.timeStatus = TIME_STATUS.PAUSE;
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.CONTINUE;
+    Timer.secondBtnStatus = SECOND_BTN_STATUS.DONE;
+
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Продолжить' : '';
+    Timer.secondBtn ? Timer.secondBtn.textContent = 'Сделано' : '';
+
     clearInterval(Timer.TimeInterval);
   }
 
   private static stop(): void {
     Timer.timeStatus = TIME_STATUS.READINESS;
-    Timer.setTime();
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.START;
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Старт' : '';
+    Timer.secondBtn ? Timer.secondBtn.disabled = true : '';
+
+    Timer.setTimeByDefault();
     clearInterval(Timer.TimeInterval);
   }
 
-  private static tick(): void {
-    clearInterval(Timer.TimeInterval);
+  private static done(): void {
+    Timer.timeEndSound.remove();
 
-    if (Timer.timeStatus === TIME_STATUS.WORK) {
+    Timer.timeStatus = TIME_STATUS.READINESS;
+
+    Task.removeTaskById(Timer.taskInfo.id);
+  }
+
+  private static tick(): void {
+    if (Timer.timeStatus === TIME_STATUS.WORK || Timer.timeStatus === TIME_STATUS.BREAK) {
+      clearInterval(Timer.TimeInterval);
+
       if (Timer.timeElement) {
-        Timer.time -= 1;
+        Timer.currentTime -= 1;
         Timer.timeElement.textContent = Timer.getNormalTimeFormat();
       }
 
       Timer.TimeInterval = setInterval(() => {
-        if (Timer.time <= 0) {
-          Timer.taskCompleted();
+        if (Timer.currentTime <= 0) {
+          if (Timer.timeStatus === TIME_STATUS.BREAK) {
+            Timer.breakCompleted();
+          } else Timer.taskCompleted();
         } else if (Timer.timeElement) {
-          Timer.time -= 1;
+          Timer.currentTime -= 1;
           Timer.timeElement.textContent = Timer.getNormalTimeFormat();
         }
       }, 1000);
     }
   }
 
-  // break
+  // break time
+
+  private static setBreakTime(seconds: number): void {
+    Timer.currentTime = seconds;
+    Timer.timeElement ? Timer.timeElement.textContent = Timer.getNormalTimeFormat() : '';
+
+    const title: HTMLTitleElement | null = document.querySelector('.app__task-title');
+    title ? title.textContent = 'Перерыв' : '';
+  }
+
   private static startBreak(): void {
+    Timer.timeEndSound.remove();
+    Timer.timeStatus = TIME_STATUS.BREAK;
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE_BREAK;
+    Timer.secondBtnStatus = SECOND_BTN_STATUS.SKIP_BREAK;
+
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Пауза' : '';
+    Timer.secondBtn ? Timer.secondBtn.textContent = 'Пропустить' : '';
+
+    Timer.addTimeBtn ? Timer.addTimeBtn.disabled = false : '';
+
+    Timer.setBreakTime(5);
+    Timer.tick();
+  }
+
+  private static continueBreak(): void {
+    Timer.timeStatus = TIME_STATUS.BREAK;
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE_BREAK;
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Пауза' : '';
+
+    Timer.tick();
+  }
+
+  private static pauseBreak(): void {
+    clearInterval(Timer.TimeInterval);
+
+    Timer.timeStatus = TIME_STATUS.BREAK;
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.CONTINUE_BREAK;
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Продолжить' : '';
 
   }
 
+  private static skipBreak(): void {
+    clearInterval(Timer.TimeInterval);
+    Timer.setTimeByDefault();
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.PAUSE;
+    Timer.secondBtnStatus = SECOND_BTN_STATUS.STOP;
+
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'пауза' : '';
+    Timer.secondBtn ? Timer.secondBtn.textContent = 'Стоп' : '';
+
+    Timer.start();
+  }
+
   private static breakCompleted(): void {
-    const currentTomato: HTMLSpanElement | null = document.querySelector('.app__task-current-tomato');
-    if (currentTomato?.textContent) {
-      currentTomato.textContent = `Помидор: ${Timer.currentTomato}`;
-    }
+    Timer.playBreakTimeEndSound();
+    Timer.setTimeByDefault();
+    clearInterval(Timer.TimeInterval);
+
+    Timer.addTimeBtn ? Timer.addTimeBtn.disabled = true : '';
+
+    gsap.set(Timer.timeElement, { fontSize: 90 });
+    Timer.timeElement ? Timer.timeElement.textContent = 'Время вышло' : '';
+
+    const title: HTMLTitleElement | null = document.querySelector('.app__task-title');
+    title ? title.textContent = 'Перерыв окончен' : '';
+
+    Timer.firstBtnStatus = FIRST_BTN_STATUS.START;
+    Timer.secondBtnStatus = SECOND_BTN_STATUS.STOP;
+
+    Timer.firstBtn ? Timer.firstBtn.textContent = 'Старт' : '';
+
+    Timer.secondBtn ? Timer.secondBtn.textContent = 'Стоп' : '';
+    Timer.secondBtn ? Timer.secondBtn.disabled = true: '';
   }
 
   // audio methods
@@ -297,5 +425,12 @@ export class Timer {
     Timer.timeEndSound.loop = true;
     document.body.append(Timer.timeEndSound);
     Timer.timeEndSound.play();
+  }
+
+  private static playBreakTimeEndSound(): void {
+    Timer.breakTimeEndSound.src = breakTimeEndSound;
+    Timer.breakTimeEndSound.loop = true;
+    document.body.append(Timer.breakTimeEndSound);
+    Timer.breakTimeEndSound.play();
   }
 }
